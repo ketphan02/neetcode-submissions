8f162a06ba69647e70faf588c001c221b854067e
Kiet Phan <35695324+ketphan02@users.noreply.github.com>
2026-08-23 03:16:32 -0700

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        m = defaultdict(list)
+        for s in strs:
+            k = ''.join(sorted(s))
+            m[k].append(s)
+        
+        return list(m.values())
+
