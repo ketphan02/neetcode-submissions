924cc2f1ef50accfa74dbd70c64b7ff28dd444af
Kiet Phan <35695324+ketphan02@users.noreply.github.com>
2026-08-26 22:26:32 -0700

    Add: house-robber - submission-6

---
+++ Data Structures & Algorithms/house-robber/submission-6.py
@@ -0,0 +1,22 @@
+from functools import cache
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+
+        @cache
+        def rob_backtrack(idx):
+            if idx >= len(nums):
+                return 0
+            return max(rob_backtrack(idx + 1), rob_backtrack(idx + 2) + nums[idx])
+        
+        if len(nums) <= 2:
+            return max(nums)
+
+        nums[1] = max(nums[0], nums[1])
+
+        for idx in range(2, len(nums)):
+            if idx - 2 < 0:
+               continue
+            nums[idx] = max(nums[idx - 2] + nums[idx], nums[idx - 1])
+        return nums[-1]
