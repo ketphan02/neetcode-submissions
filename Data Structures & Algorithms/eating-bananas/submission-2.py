2f1fd6ef3bd13dc5616932865f250f67e4b82018
Kiet Phan <35695324+ketphan02@users.noreply.github.com>
2026-08-28 22:22:33 -0700

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.py
@@ -0,0 +1,20 @@
+import math
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def can_finish(k):
+            total = 0
+            for b in piles:
+                total += math.ceil(b / k)
+                if total > h:
+                    return False
+            return True
+    
+        piles.sort()
+        l, r = 1, piles[-1]
+        while l < r:
+            m = (l + r) // 2
+            if not can_finish(m):
+                l = m + 1
+            else:
+                r = m
+        return r
